cb4d4c4d60dafd2f0c2c3de9098d4d7af5e72ae4
Abhinav Jambulingam <abhij1@umbc.edu>
2022-05-21 02:22:00 -0400

    Working First Page

--- exchange_log/pages/index.js
+++ exchange_log/pages/index.js
@@ -4,7 +4,34 @@ const CoinGecko = require('coingecko-api');
 export default function Home() {
   const [exchanges, setExchanges] = useState([]);
   const CoinGeckoClient = new CoinGecko();
-     
+  useEffect(() => {
+    var func = async () => {
+      let data = await CoinGeckoClient.exchanges.all();
+      return data;
+    };
+    
+    func()
+    .then((res) => {
+      setExchanges([...res.data])
+    });
+  },[]);
+  const tableData = () => {
+    const table = [];
+    if(exchanges.length > 0){
+    for(var c = 0; c < 10; c++){
+      table.push(
+        <TableRow id={exchanges[c].id} hover onClick={(e) => {console.log(exchanges[c].id);
+        window.location = `/exchange?id=${exchanges[c].id}`}}>
+        <TableCell><Avatar src={exchanges[c].image}/></TableCell>
+        <TableCell>{exchanges[c].name}</TableCell>
+        <TableCell>{exchanges[c].country}</TableCell>
+        <TableCell>{exchanges[c].trust_score_rank}</TableCell>
+        </TableRow>)
+    }
+    }
+    return table;
+  }
+    
   return (<>
     <div><Typography variant="h5" gutterBottom component="div">Exchanges:</Typography></div>
     <br />
@@ -19,6 +46,7 @@ export default function Home() {
         </TableRow>
         </TableHead>
         <TableBody>
+        {tableData()}
         </TableBody>
       </Table>
     </TableContainer>
